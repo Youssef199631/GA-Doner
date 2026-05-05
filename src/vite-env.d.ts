620a1b125e8230b8995fecf65799c0b95cf2bc8e
Youssef199631 <ouazzani9633@gmail.com>
2026-05-05 21:11:41 +0200

    feat: Import assets using Vite
    Moves image assets into the `src/assets` directory and updates imports to use Vite's asset handling. This ensures proper asset management and optimizes loading.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,6 +10,11 @@ declare module "*.jpg" {
   export default value;
 }
 
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
 declare module "*.svg" {
   const value: any;
   export default value;
